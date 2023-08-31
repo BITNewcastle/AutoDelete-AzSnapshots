aaccde39ca1b178f031436a62c86feca1a7c81a9
BITNewcastle <137378485+BITNewcastle@users.noreply.github.com>
2023-08-31 16:42:56 +1000

    Update Remove-AzSnapshotAutomation.ps1

--- Remove-AzSnapshotAutomation.ps1
+++ Remove-AzSnapshotAutomation.ps1
@@ -5,7 +5,7 @@
         This script is deployed via Bicep, in the form of a PowerShell Runbook under an Automation Account running on a recurring schedule.
         The Automation Account requires a system assigned managed identity. The identity requires 'Disk Snapshot Contributor' RBAC permissions assigned to the tenant's Azure subscription.
         If you are running this script ad-hoc and not in the context of a runbook, simply comment out the initialisations, and run Connect-AzAccount and sign in as a tenant admin with appropriate RBAC permissions, prior to running script.
-        It first initialises the connection to Azure with the system assigned managed identity.       
+        It first initialises the connection to Azure with the system assigned managed identity.
         It then gets and iterates through all Azure snapshots. If the snapshot meets the conditions (created 3+ days ago and does not have the excusion Azure Tags applied (snapshotLock:doNotDelete)), it then deletes the snapshot.
     .NOTES
         AUTHOR: Christopher Cooper
@@ -22,24 +22,32 @@ $azureContext = Set-AzContext -SubscriptionName $azureContext.Subscription -Defa
 
 ####----------------[SCRIPT]----------------####
 
+Write-Output "Running automation..."
 # Get all snapshots
 $snapshots = Get-AzSnapshot
+# If no snapshots present, log output and exit
+if (!$snapshots) {
+    Write-Output "Currently no snapshots present, no changes made"
+    Exit 0
+}
 # Iterate through each snapshot
 foreach($snapshot in $snapshots) {
+    Write-Output "Analysing $($snapshot.Name)"
     # Get snapshot's Azure tags
     $snapshotTags = (Get-AzResource -Name $snapshot.Name).Tags
-    # If snapshot is within 3 days old, output that it has been skipped
-    if ($snapshot.TimeCreated -gt ([datetime]::UtcNow.AddDays(-3))) {
-        Write-Output "Snapshot $($snapshot.Name) is not due for deletion yet as it is not older than 3 days, skipping snapshot"
-    }
-    # If snapshot is 3+ days old and has the Azure tag applied to exclude from deletion, output that it has been excluded
-    if ($snapshot.TimeCreated -lt ([datetime]::UtcNow.AddDays(-3)) -and $snapshotTags.snapshotLock -eq 'doNotDelete') {
-        Write-Output "Snapshot $($snapshot.Name) is due for deletion as it is older than 3 days, but was excluded. It is currently marked for exclusion from deletion via Azure tag 'snapshotLock:doNotDelete' currently applied on snapshot"
-    }
     # If snapshot is 3+ days old and does not have the Azure tag applied to exclude from deletion, delete snapshot
     if ($snapshot.TimeCreated -lt ([datetime]::UtcNow.AddDays(-3)) -and $snapshotTags.snapshotLock -ne 'doNotDelete') {       
         # Delete snapshot
         Write-Output "Snapshot $($snapshot.Name) is due for deletion as it is older than 3 days, deleting snapshot"
         Get-AzSnapshot -SnapshotName $snapshot.Name | Remove-AzSnapshot -Verbose -Force
+    }
+    ## Log output if snapshot has been skipped or excluded due to not meeting conditions for deletion
+    # If snapshot is within 3 days old and has been skipped, log output
+    elseif ($snapshot.TimeCreated -gt ([datetime]::UtcNow.AddDays(-3))) {
+        Write-Output "Snapshot $($snapshot.Name) is not due for deletion yet as it is not older than 3 days, skipping snapshot"
+    }
+    # If snapshot is 3+ days old and has the Azure tag applied to exclude from deletion and has been excluded, log output
+    elseif ($snapshot.TimeCreated -lt ([datetime]::UtcNow.AddDays(-3)) -and $snapshotTags.snapshotLock -eq 'doNotDelete') {
+        Write-Output "Snapshot $($snapshot.Name) is due for deletion as it is older than 3 days, but was excluded. It is currently marked for exclusion from deletion via Azure tags snapshotLock:doNotDelete currently applied on snapshot"
     }
 }
