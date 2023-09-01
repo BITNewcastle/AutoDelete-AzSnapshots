2f7d7fcefb1018d6e1b6efc9f91e4b4c66e23dc4
BITNewcastle <137378485+BITNewcastle@users.noreply.github.com>
2023-09-01 11:44:48 +1000

    Update Remove-AzSnapshotAutomation.ps1

--- Remove-AzSnapshotAutomation.ps1
+++ Remove-AzSnapshotAutomation.ps1
@@ -53,7 +53,7 @@ else {
         }
         # If snapshot is 3+ days old and has the Azure tag applied to exclude from deletion and has been excluded, log output
         elseif ($snapshot.TimeCreated -lt ([datetime]::UtcNow.AddDays(-3)) -and $snapshotTags.snapshotLock -eq 'doNotDelete') {
-            Write-Output "Snapshot $($snapshot.Name) is due for deletion as it is older than 3 days, but was excluded. It is excluded from deletion via Azure tags snapshotLock:doNotDelete currently applied on snapshot."
+            Write-Output "Snapshot $($snapshot.Name) is due for deletion as it is older than 3 days, but was excluded via Azure tags snapshotLock:doNotDelete currently applied on snapshot"
         }
     }
     Write-Output "Automation complete, exiting"
